da40351cb89e202b600d22ac5d3a24cc3c2b06dd
Kostas Mamalis <kostas@masterthought.net>
2017-11-08 12:26:38 +0000

    feat(ui): integrating with the graphql

---
+++ lambdas/app-client/src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import UserList from './components/UserList';
+import logo from './logo.svg';
+import './App.css';
+
+import {
+  ApolloClient,
+  ApolloProvider,
+  createNetworkInterface,
+} from 'react-apollo';
+
+const networkInterface = createNetworkInterface({
+  uri: process.env.REACT_APP_GRAPHQL_ENDPOINT || 'https://xb9vxxznul.execute-api.us-east-1.amazonaws.com/production/graphql',
+});
+
+const client = new ApolloClient({
+  networkInterface,
+});
+
+class App extends Component {
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <div className="App">
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Serverless GraphQL Apollo </h2>
+          </div>
+          <p className="App-intro">
+            Welcome to the world of <code>Serverless</code> and{' '}
+            <code>GraphQL</code>.
+          </p>
+          <p className="App-User">
+            <UserList />
+          </p>
+        </div>
+      </ApolloProvider>
+    );
+  }
+}
+
+export default App;
